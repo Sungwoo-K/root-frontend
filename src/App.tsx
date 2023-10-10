75447c6899023fd134a8f9dc0e9689cda4e3b781
Sungwoo-K <72499989+Sungwoo-K@users.noreply.github.com>
2023-10-10 12:53:25 +0900

    React team project 초기환경설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./modules/main/Layout";
+import MarketLayout from "./modules/market/Layout";
+import CommunityLayout from "./modules/community/Layout";
+import { mainRoutes } from "./modules/main/routes";
+import { communityRoutes } from "./modules/community/routes";
+import { productRoutes } from "./modules/market/productRoutes";
+import { userRoutes } from "./modules/market/userRoutes";
+
 const App = () => {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          {mainRoutes}
+        </Route>
+        <Route element={<MarketLayout />} path="/market">
+          {productRoutes}
+          {userRoutes}
+        </Route>
+        <Route element={<CommunityLayout />} path="/community">
+          {communityRoutes}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
