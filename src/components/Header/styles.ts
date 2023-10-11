8149135608b0945ae5581e3fd09709039fdfcaa9
GangChan Park <sunset2350@naver.com>
2023-10-11 11:53:36 +0900

    불필요 파일 제거

--- src/components/Header/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Header = styled.header`
-  height: 80px;
-  width: 100vw;
-`;
-
-export const Logoimage = styled.image``;
--- src/components/Sidebar/styles.ts
+++
@@ -1,8 +0,0 @@
-// import styled from "@emotion/styled";
-
-// export const Sidebar = styled.div`
-//   background-color: green;
-//   height: 100%;
-//   width: 200px;
-//   position: fixed;
-// `;
