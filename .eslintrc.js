03043c295d773058dddff6cf19e85c1b0fcc8ef0
Sungwoo-K <72499989+Sungwoo-K@users.noreply.github.com>
2023-10-18 16:06:56 +0900

    Add: esline설치 및 설정

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+  },
+};
--- src/modules/market/productRoutes.tsx
+++ src/modules/market/productRoutes.tsx
@@ -1,4 +1,6 @@
 import { Route } from "react-router-dom";
 import MarketMain from "./product/Main";
 
-export const productRoutes = [<Route element={<MarketMain />} index />];
+export const productRoutes = [
+  <Route key="main" element={<MarketMain />} index />,
+];
