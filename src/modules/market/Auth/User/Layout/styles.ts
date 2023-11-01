e09186b7ae4138981345a6dfdfee9bc5ef4440e6
GangChan <sunset2350@naver.com>
2023-11-02 01:29:46 +0900

    update profile

--- src/modules/market/Auth/User/Layout/styles.ts
+++ src/modules/market/Auth/User/Layout/styles.ts
@@ -1,29 +1,29 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  display: flex;
-  margin-top: 10vh;
-  align-items: center;
-  justify-content: space-between;
-  gap: 16%;
-  height: 10%;
-  position: fixed;
-  top: 0;
-  left: 10%;
-  right: 0;
-
-  width: 80%;
-
-  .Home,
-  .Scrap,
-  .Great,
-  .Setting {
-    width: 7%;
-    height: 70%;
-    border: 1px solid;
-    border-radius: 10px;
     display: flex;
+    margin-top: 10vh;
     align-items: center;
-    justify-content: center;
-  }
+    justify-content: space-between;
+    gap: 16%;
+    height: 10%;
+    position: fixed;
+    top: 0;
+    left: 10%;
+    right: 0;
+
+    width: 80%;
+
+    .Home,
+    .Scrap,
+    .Great,
+    .Setting {
+        width: 7%;
+        height: 70%;
+        border: 1px solid;
+        border-radius: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 `;
