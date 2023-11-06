dedbfd7891e4cfb4659a3839413f2ad2a1362039
GangChan Park <sunset2350@naver.com>
2023-11-06 18:27:33 +0900

    update merge

--- src/modules/market/Auth/Join/index.tsx
+++ src/modules/market/Auth/Join/index.tsx
@@ -1,143 +1,146 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import { Container, Forminput, Input, Inputdiv, Inputp } from './style';
-import axios from 'axios';
-import { MutableRefObject, useRef, useState } from 'react';
-import { Button } from '../Login/styles';
+import { Container, Forminput, Input, Inputdiv, Inputp } from "./style";
+import axios from "axios";
+import { MutableRefObject, useRef, useState } from "react";
+import { Button } from "../Login/styles";
 
 const Join = () => {
-    const [formData, setFormData] = useState({
-        username: '',
-        userid: '',
-        userpassword: '',
-        nickname: '',
-        selctSex: '',
-        userbirth: '',
+  const [formData, setFormData] = useState({
+    username: "",
+    userid: "",
+    userpassword: "",
+    nickname: "",
+    selctSex: "",
+    birth: "",
+  });
+
+  const value = ["남성", "여성"];
+  const [selctSex, isSelectSex] = useState("");
+
+  const clickButton = (e) => {
+    isSelectSex(() => {
+      return e.target.value;
+    });
+  };
+
+  const postExample = async (e) => {
+    e.preventDefault();
+
+    const response = await axios.post("http://localhost:8080/auth/sign", {
+      userid: formData.userid,
+      username: formData.username,
+      userpassword: formData.userpassword,
+      nickname: formData.nickname,
+      usersex: formData.selctSex,
+      birth: formData.birth,
     });
 
-    const value = ['남성', '여성'];
-    const [selctSex, isSelectSex] = useState('');
+    if (response.status === 201) {
+      alert("회원가입이 성공했습니다.");
+      window.location.href = "/login";
+    } else {
+      alert("회원가입에 실패했습니다.");
+    }
+  };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
 
-    const clickButton = (e) => {
-        isSelectSex(() => {
-            return e.target.value;
-        });
-    };
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-    const postExample = async (e) => {
-        e.preventDefault();
+  return (
+    <>
+      <Container>
+        <Link
+          to="http://localhost:5000/"
+          style={{
+            display: "flex",
+            width: "100%",
+            alignContent: "center",
+          }}
+        >
+          <div style={{ marginTop: "10px", marginLeft: "30px" }}>
+            <img
+              src={require("@/components/market/Header/tent.png")}
+              style={{
+                width: "50px",
+                height: "50px",
+                marginRight: "10px",
+              }}
+            />
+            <img
+              src={require("@/components/market/Header/logo.png")}
+              style={{ width: "160px", height: "55px" }}
+            />
+          </div>
+        </Link>
 
-        const response = await axios.post('http://localhost:8080/auth/sign', {
-            userid: formData.userid,
-            username: formData.username,
-            userpassword: formData.userpassword,
-            nickname: formData.nickname,
-            usersex: formData.selctSex,
-            userbirth: formData.userbirth,
-        });
+        <p style={{ fontSize: "40px", marginTop: "70px" }}> 회원 가입</p>
 
-        if (response.status === 201) {
-            alert('회원가입이 성공했습니다.');
-            window.location.href = '/login';
-        } else {
-            alert('회원가입에 실패했습니다.');
-        }
-    };
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
+        <Input
+          name="userid"
+          value={formData.userid}
+          onChange={handleInputChange}
+          placeholder="아이디"
+          style={{ marginTop: "70px" }}
+        ></Input>
 
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
-    };
+        <Input
+          type="password"
+          name="userpassword"
+          value={formData.userpassword}
+          onChange={handleInputChange}
+          placeholder="패스워드"
+          style={{ marginBottom: "20px" }}
+        ></Input>
 
-    return (
-        <>
-            <Container>
-                <Link
-                    to="http://localhost:5000/"
-                    style={{
-                        display: 'flex',
-                        width: '100%',
-                        alignContent: 'center',
-                    }}
-                >
-                    <div style={{ marginTop: '10px', marginLeft: '30px' }}>
-                        <img
-                            src={require('@/components/market/Header/tent.png')}
-                            style={{
-                                width: '50px',
-                                height: '50px',
-                                marginRight: '10px',
-                            }}
-                        />
-                        <img
-                            src={require('@/components/market/Header/logo.png')}
-                            style={{ width: '160px', height: '55px' }}
-                        />
-                    </div>
-                </Link>
+        <Input
+          name="username"
+          value={formData.username}
+          onChange={handleInputChange}
+          placeholder="이름"
+        ></Input>
 
-                <p style={{ fontSize: '40px', marginTop: '70px' }}> 회원 가입</p>
+        <Input
+          name="nickname"
+          value={formData.nickname}
+          onChange={handleInputChange}
+          placeholder="닉네임"
+        ></Input>
 
-                <Input
-                    name="userid"
-                    value={formData.userid}
-                    onChange={handleInputChange}
-                    placeholder="아이디"
-                    style={{ marginTop: '70px' }}
-                ></Input>
-
-                <Input
-                    type="password"
-                    name="userpassword"
-                    value={formData.userpassword}
-                    onChange={handleInputChange}
-                    placeholder="패스워드"
-                    style={{ marginBottom: '20px' }}
-                ></Input>
-
-                <Input
-                    name="username"
-                    value={formData.username}
-                    onChange={handleInputChange}
-                    placeholder="이름"
-                ></Input>
-
-                <Input
-                    name="nickname"
-                    value={formData.nickname}
-                    onChange={handleInputChange}
-                    placeholder="닉네임"
-                ></Input>
-
-                <Input
-                    type="text"
-                    placeholder="생년월일 8자리"
-                    name="userbirth"
-                    value={formData.userbirth}
-                    onChange={handleInputChange}
-                ></Input>
-                <div className="select-sex">
-                    {value.map((item, idx) => (
-                        <button
-                            key={idx}
-                            value={item}
-                            name="usersex"
-                            className={'buttonevent' + (item == selctSex ? idx + 1 : '')}
-                            onClick={clickButton}
-                            onChange={handleInputChange}
-                        >
-                            {item}
-                        </button>
-                    ))}
-                </div>
-                <Button onClick={postExample} style={{ width: '31vh', marginTop: '15vh' }}>
-                    회원가입하기
-                </Button>
-            </Container>
-        </>
-    );
+        <Input
+          type="text"
+          placeholder="생년월일 8자리"
+          name="birth"
+          value={formData.birth}
+          onChange={handleInputChange}
+        ></Input>
+        <div className="select-sex">
+          {value.map((item, idx) => (
+            <button
+              key={idx}
+              value={item}
+              name="usersex"
+              className={"buttonevent" + (item == selctSex ? idx + 1 : "")}
+              onClick={clickButton}
+              onChange={handleInputChange}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+        <Button
+          onClick={postExample}
+          style={{ width: "31vh", marginTop: "15vh" }}
+        >
+          회원가입하기
+        </Button>
+      </Container>
+    </>
+  );
 };
 export default Join;
--- src/modules/market/userRoutes.tsx
+++ src/modules/market/userRoutes.tsx
@@ -1,42 +1,50 @@
-import { Route } from 'react-router-dom';
-import MarketMain from './product/Main';
-import { lazy } from 'react';
-import Join from './auth/Join';
+import { Route } from "react-router-dom";
+import MarketMain from "./product/Main";
+import { lazy } from "react";
+import Join from "./auth/Join";
 
-const Users = lazy(() => import('@/modules/market/auth/User/Profile'));
-const Follow = lazy(() => import('@/modules/market/auth/User/ProfileTop'));
-const Layout = lazy(() => import('@/modules/market/auth/User/Shopping/Layout'));
-const EditProfile = lazy(() => import('@/modules/market/auth/User/Edit/'));
-const Shoppinghistory = lazy(() => import('@/modules/market/auth/User/Shopping'));
-const Orderdetail = lazy(() => import('@/modules/market/auth/User/Shopping/Order'));
-const Reviewdetail = lazy(() => import('@/modules/market/auth/User/Shopping/Review'));
-const MyReview = lazy(() => import('@/modules/market/auth/User/Shopping/My-review'));
-const Inquery = lazy(() => import('@/modules/market/auth/User/Inquery'));
+const Users = lazy(() => import("@/modules/market/auth/User/Profile"));
+const Follow = lazy(() => import("@/modules/market/auth/User/ProfileTop"));
+const Layout = lazy(() => import("@/modules/market/auth/User/Shopping/Layout"));
+const EditProfile = lazy(() => import("@/modules/market/auth/User/Edit/"));
+const Shoppinghistory = lazy(
+  () => import("@/modules/market/auth/User/Shopping")
+);
+const Orderdetail = lazy(
+  () => import("@/modules/market/auth/User/Shopping/Order")
+);
+const Reviewdetail = lazy(
+  () => import("@/modules/market/auth/User/Shopping/Review")
+);
+const MyReview = lazy(
+  () => import("@/modules/market/auth/User/Shopping/My-review")
+);
+const Inquery = lazy(() => import("@/modules/market/auth/User/Inquery"));
 
-const Favorite = lazy(() => import('@/modules/market/auth/User/Favorite'));
-const Scrap = lazy(() => import('@/modules/market/auth/User/Favorite/Scrap'));
+const Favorite = lazy(() => import("@/modules/market/auth/User/Favorite"));
+const Scrap = lazy(() => import("@/modules/market/auth/User/Favorite/Scrap"));
 
 export const userRoutes = [
-    <Route key="main" element={<MarketMain />} index />,
-    <Route path="/user" key="user" element={<Layout />}>
-        <Route path="profile" key="profile" element={<Users />}>
-            <Route key="follow" element={<Follow />} index />
-            <Route path="edits" element={<EditProfile />} />
-        </Route>
-    </Route>,
+  <Route key="main" element={<MarketMain />} index />,
+  <Route path="/user" key="user" element={<Layout />}>
+    <Route path="profile" key="profile" element={<Users />}>
+      <Route key="follow" element={<Follow />} index />
+      <Route path="edits" element={<EditProfile />} />
+    </Route>
+  </Route>,
 
-    <Route path="/myorder" key="order" element={<Layout />}>
-        <Route path="list" key="order-list" element={<Shoppinghistory />}>
-            <Route path="detail" key="order-detail" element={<Orderdetail />} />
-            <Route path="review" key="review-detail" element={<Reviewdetail />} />
-            <Route path="my-review" key="my-review" element={<MyReview />} />
-            <Route path="inquery" key="my-inquery" element={<Inquery />} />
-        </Route>
-    </Route>,
+  <Route path="/myorder" key="order" element={<Layout />}>
+    <Route path="list" key="order-list" element={<Shoppinghistory />}>
+      <Route path="detail" key="order-detail" element={<Orderdetail />} />
+      <Route path="review" key="review-detail" element={<Reviewdetail />} />
+      <Route path="my-review" key="my-review" element={<MyReview />} />
+      <Route path="inquery" key="my-inquery" element={<Inquery />} />
+    </Route>
+  </Route>,
 
-    <Route path="/myfavorite" key="favorite" element={<Layout />}>
-        <Route path="list" key="favorite-list" element={<Favorite />}>
-            <Route path="scrap" key="scrap-list" element={<Scrap />} />
-        </Route>
-    </Route>,
+  <Route path="/myfavorite" key="favorite" element={<Layout />}>
+    <Route path="list" key="favorite-list" element={<Favorite />}>
+      <Route path="scrap" key="scrap-list" element={<Scrap />} />
+    </Route>
+  </Route>,
 ];
