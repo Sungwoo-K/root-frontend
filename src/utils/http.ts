94b06b33bb69ce0ccf8917232cdd6e45068e2ed5
GangChan <sunset2350@naver.com>
2023-11-07 01:18:22 +0900

    join update

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,46 +1,41 @@
-import axios from "axios";
-import { getCookie } from "./cookie";
+import axios from 'axios';
+import { getCookie } from './cookie';
 
 const http = axios.create();
 
-// 요청값에 대해서 사전처리
 http.interceptors.request.use((config) => {
-  const token = getCookie("token");
-  console.log("---token---");
-  console.log(token);
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+    const token = getCookie('token');
+    console.log('---token---');
+    console.log(token);
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
-// 응답값에 대해서 사전처리
 http.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response) {
-      const { status } = error.response;
+    (response) => response,
+    (error) => {
+        if (error.response) {
+            const { status } = error.response;
 
-      if (status === 401 || status == 403) {
-        alert("로그인이 필요합니다");
+            if (status === 401 || status == 403) {
+                alert('로그인이 필요합니다');
 
-        window.location.href = "/login";
-      }
+                window.location.href = '/login';
+            }
 
-      if (status === 404) {
-        alert("데이터 존재하지 않습니다.");
-      } else {
-        console.error("No response found in error object:", error);
-      }
-    } else {
-      console.error(
-        "Error object does not contain a response property:",
-        error
-      );
+            if (status === 404) {
+                alert('데이터 존재하지 않습니다.');
+            } else {
+                console.error('No response found in error object:', error);
+            }
+        } else {
+            console.error('Error object does not contain a response property:', error);
+        }
+
+        return;
     }
-
-    return;
-  }
 );
 
 export default http;
